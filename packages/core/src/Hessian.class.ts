c7a4b68f2e974840ffa88671918b0852368270ec
no4any <martin@karry.de>
2025-04-18 19:40:31 +0200

    Add doc with static pages

--- packages/core/src/Hessian.class.ts
+++ packages/core/src/Hessian.class.ts
@@ -4,4 +4,8 @@ export class Hessian {
     public getId() {
         return this.id;
     }
+
+    public test() {
+        return "test123";
+    }
 }
---
+++ packages/doc/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div>
+    <h1>Hessian CMS</h1>
+  </div>
+}
