e4f500d2208e079e4a7edb1b51e616412e1dd551
no4any <martin@karry.de>
2025-11-26 21:46:44 +0100

    Removed generateId

--- packages/pathway/src/generateId.function.ts
+++
@@ -1,7 +0,0 @@
-export function generateMongoLikeId() {
-  const timestamp = Math.floor(Date.now() / 1000).toString(16);
-  const randomHex = 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, () => {
-    return Math.floor(Math.random() * 16).toString(16);
-  });
-  return timestamp + randomHex;
-}
--- packages/pathway/src/index.ts
+++ packages/pathway/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./ndjson";
 export * from "./errors";
 export * from "./models";
-export * from "./generateId.function";
 import Pathway from "./Pathway.class";
 
 export default Pathway;
