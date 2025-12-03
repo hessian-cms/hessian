df90b364031f4da592723b414f2eaf9c03f1d6cb
no4any <martin@karry.de>
2025-12-03 21:37:52 +0100

    Configuring jest to come with default configuration

--- packages/ichabod/jest.config.js
+++ packages/ichabod/jest.config.js
@@ -1,19 +1,6 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
+const defaultConfig = require("../../jest.config.base.js");
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    }
-  },
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  ...defaultConfig
 };
