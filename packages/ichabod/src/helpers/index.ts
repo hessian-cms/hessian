1853452fae9cfec802324ab477ff082540ba4ea8
no4any <martin@karry.de>
2026-02-22 14:34:51 +0100

    Added Mutex2 with tests

--- packages/ichabod/src/helpers/index.ts
+++ packages/ichabod/src/helpers/index.ts
@@ -1,1 +1,2 @@
 export * from "./Mutex.class";
+export * from "./Mutex2.class";
--- packages/ichabod/test/Mutex.class.test.ts
+++ packages/ichabod/test/Mutex.class.test.ts
@@ -1,4 +1,6 @@
-import { Mutex } from "../src/helpers/Mutex.class";
+import { Mutex, Mutex2 } from "../src/helpers";
+
+const expectedResults: number[] = [1, 2, 3];
 
 const executeAfterTime = (time: number, func: () => void) => {
     return new Promise<void>(resolve => setTimeout(() => {
@@ -7,20 +9,20 @@ const executeAfterTime = (time: number, func: () => void) => {
     }, time));
 }
 
-const mutex = new Mutex();
+[Mutex, Mutex2].forEach((MutexClass) => {
+    const mutex = new MutexClass();
+    const mutexedExectuteAfterTime = mutex.lock(executeAfterTime);
 
-const mutexedExectuteAfterTime = mutex.lock(executeAfterTime);
-
-describe("Mutex class", () => {
-    test("Testing", async () => {
-        expect(1);
-
-        const arr: number[] = [];
-
-        mutexedExectuteAfterTime(500, () => { arr.push(1); });
-        mutexedExectuteAfterTime(250, () => { arr.push(2); });
-        await mutexedExectuteAfterTime(125, () => { arr.push(3); });
-
-        expect(arr).toEqual([1, 2, 3]);
-    })
-})
+    describe(`${MutexClass.name} class`, () => {
+        test("Testing", async () => {
+            expect(1);
+            const arr: number[] = [];
+            const promises: Promise<void>[] = [];
+            promises.push(mutexedExectuteAfterTime(500, () => { arr.push(1); }));
+            promises.push(mutexedExectuteAfterTime(250, () => { arr.push(2); }));
+            promises.push(mutexedExectuteAfterTime(125, () => { arr.push(3); }));
+            await Promise.all(promises);
+            expect(arr).toEqual(expectedResults);
+        });
+    });
+});
