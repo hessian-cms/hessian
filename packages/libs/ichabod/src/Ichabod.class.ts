129af6430a7415822ee4fff280935029415c86f2
no4any <martin@karry.de>
2026-04-04 09:37:23 +0200

    Added bindings for mutex

--- packages/libs/ichabod/src/Ichabod.class.ts
+++ packages/libs/ichabod/src/Ichabod.class.ts
@@ -1,12 +1,23 @@
+import { Mutex2 } from "./helpers";
 import { Event, WithId } from "./models";
 import { InMemoryTrailStorage, TrailStorage } from "./trailStorage";
 
-export default class Ichabod {
+export default class Ichabod<T extends Event> {
+    private mutex = new Mutex2();
+
     private state: Map<string, WithId> = new Map<string, WithId>();
 
-    private constructor(private trailStorage: TrailStorage<Event>) { }
+    private constructor(private trailStorage: TrailStorage<T>) {
+        this.findOne = this.mutex.lock(this.findOne.bind(this));
+        this.find = this.mutex.lock(this.find.bind(this));
+        this.deleteOne = this.mutex.lock(this.deleteOne.bind(this));
+        this.delete = this.mutex.lock(this.delete.bind(this));
+        this.updateOne = this.mutex.lock(this.updateOne.bind(this));
+        this.update = this.mutex.lock(this.update.bind(this));
+        this.insert = this.mutex.lock(this.insert.bind(this));
+    }
 
-    public static async getInstance(trailStorage: TrailStorage<Event> = new InMemoryTrailStorage()): Promise<Ichabod> {
+    public static async getInstance<T extends Event>(trailStorage: TrailStorage<T> = new InMemoryTrailStorage()): Promise<Ichabod<T>> {
         const ichabod = new Ichabod(trailStorage);
         await ichabod.build();
         return ichabod;
@@ -35,6 +46,20 @@ export default class Ichabod {
             };
         });
     }
+
+    public async findOne() {}
+
+    public async find() {}
+
+    public async deleteOne() {}
+
+    public async delete() {}
+
+    public async updateOne() {}
+
+    public async update() {}
+
+    public async insert() {}
 
     public async getState() {
         return this.state;
